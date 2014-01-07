e64a3e0ab0bb4306e5df783c5a43be8b5046b924
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-06 18:09:35 -0800

    Update build script, autoadd generated stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
+rm config.yaml INFO
 rm -rf node_modules
 npm install sinopia
-echo "Please hit Ctrl-C to generate config.yaml and README"
-yes | node_modules/sinopia > INFO
+yes Y | node_modules/sinopia/bin/sinopia > INFO &
+pid=$!
+sleep 1 # Let it make the config
+sed 's/\#listen\: localhost/listen\: 0.0.0.0/' config.yaml > tmp.yaml
+kill -9 $pid
+rm config.yaml
+mv tmp.yaml config.yaml
 sudo docker build -t keyvanfatehi/docker-sinopia .
+echo publish with sudo docker push keyvanfatehi/docker-sinopia
