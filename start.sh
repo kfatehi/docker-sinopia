e8930527afe5fc8d0290d79e2f6117dbdf0b5d7c
Kurt Harriger <kurtharriger@gmail.com>
2014-08-26 21:01:36 -0600

    Only generate config file if it does not already exist

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-nodejs /opt/sinopia/config_gen.js
-cat /tmp/config.yaml
-sed 's/\#listen\: localhost/listen\: 0.0.0.0/' /tmp/config.yaml > /opt/sinopia/config.yaml
+if [ ! -f config.yaml ]; then
+  nodejs /opt/sinopia/config_gen.js
+  sed -e 's/\#listen\: localhost/listen\: 0.0.0.0/' /tmp/config.yaml > /opt/sinopia/config.yaml
+fi
+cat /opt/sinopia/config.yaml
 nodejs /node_modules/sinopia/bin/sinopia --config /opt/sinopia/config.yaml
