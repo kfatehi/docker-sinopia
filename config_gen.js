259d4b44874e1b12cbb9d01bb2d1fe57f3fc2d4a
Baptiste GAILLARD <baptiste.gaillard@gomoob.com>
2015-09-30 01:34:36 +0200

    Fix for sinopia default config
    This pull request allow several improvements :
    * Removes the config_gen.js file and replaces it by a config.yaml file
    which is the same as the default sinopia default.yaml file but with a
    0.0.0.0:4873 listen instruction ;
    * Use the node Docker image instead of Dockerfile/nodejs
    * The updates should allow to pull the last version of sinopia (1.4.x)
    
    I think this fixes issues #15, #16, #17 and #18.

--- config_gen.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-var config = require('sinopia/lib/config_gen')()
-  , fs = require('fs')
-
-console.log('Username: %s', config.user)
-console.log('Password: %s', config.pass)
-fs.writeFileSync('/tmp/config.yaml', config.yaml)
